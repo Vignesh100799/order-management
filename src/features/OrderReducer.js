ed0a92049dc16be7c1c09436cdb6a996f080f93c
naveenkarmegam <naveenkkn03@gmail.com>
2023-12-08 12:39:36 +0530

    reducer upated

--- src/features/OrderReducer.js
+++ src/features/OrderReducer.js
@@ -1,33 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-<<<<<<< HEAD:src/features/UserReducer.jsx
-export const userSlice = createSlice({
-  name: "order_list",
-  initialState: {
-    orders: [],
-    loading: false,
-  },
-  reducers: {
-    setOrder: (state, action) => {
-      state.orders = action.payload;
-      state.loading = false;
-      return state;
-    },
-    createOder: (state, action) => {
-      state.orders = [...state.orders, action.payload];
-      state.loading = false;
-      return state;
-    },
-
-    editOrder: (state, action) => {
-      const updatedOrder = action.payload;
-      const index = state.orders.findIndex(
-        (order) => order.id === updatedOrder.id
-      );
-      if (index !== -1) {
-        state.orders[index] = updatedOrder;
-      }
-=======
 export const orderSlice = createSlice({
     name: "order_list",
     initialState: {
@@ -73,39 +45,10 @@ export const orderSlice = createSlice({
         setLoading: (state) => {
             state.loading = true;
         },
->>>>>>> 92c03fa2202bf226e9407e47af6b3a732b2651bc:src/features/OrderReducer.js
     },
-    lookOrder: (state, action) => {
-      state.orders = action.payload;
-      state.loading = false;
-      return state;
-    },
-    deleteOrder: (state, action) => {
-      const id = action.payload;
-      state.orders = state.orders.filter((details) => details.id !== id);
-      return state;
-    },
-    setSideBarToggle: (state, action) => {
-      state.sideBarToggle = action.payload;
-    },
-    setLoading: (state) => {
-      state.loading = true;
-    },
-  },
-  sideBarToggle: true,
+    sideBarToggle: true,
 });
 export const {
-<<<<<<< HEAD:src/features/UserReducer.jsx
-  setSideBarToggle,
-  sideBarToggle,
-  setOrder,
-  createOder,
-  editOrder,
-  deleteOrder,
-  setLoading,
-  lookOrder,
-} = userSlice.actions;
-=======
     setSideBarToggle,
     sideBarToggle,
     setOrder,
@@ -115,6 +58,5 @@ export const {
     deleteOrder,
     setLoading
 } = orderSlice.actions;
->>>>>>> 92c03fa2202bf226e9407e47af6b3a732b2651bc:src/features/OrderReducer.js
 
 export default orderSlice.reducer;
