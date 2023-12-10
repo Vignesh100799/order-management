fd9685cd4e78647c2b88422c62300000eb4dcbb5
naveenkarmegam <naveenkkn03@gmail.com>
2023-12-10 11:40:40 +0530

    update

--- src/core/Community.jsx
+++ src/core/Community.jsx
@@ -1,49 +1,48 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from './layout/Layout';
 import axios from 'axios';
 import { SendFill } from 'react-bootstrap-icons';
-import { useFormik } from 'formik';
-import { useDispatch, useSelector } from 'react-redux';
-import { ftechFeedback, setFeedback } from '../features/UserReducer';
 
 const Community = () => {
-  const { feedback } = useSelector((state) => state.users_info);
-  const dispatch = useDispatch();
-  const messagesContainerRef = useRef();
+  const [feedback, setFeedback] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
 
-  const formik = useFormik({
-    initialValues: {
-      messages: '',
-    },
-    onSubmit: async (values) => {
+  useEffect(() => {
+    const getData = async () => {
       try {
-        const response = await axios.post('https://65615e6adcd355c08323c948.mockapi.io/users', values);
-        dispatch(setFeedback(response.data));
-        formik.resetForm();
+        const response = await axios.get('https://65615e6adcd355c08323c948.mockapi.io/users');
+        setFeedback(response.data);
       } catch (error) {
         console.log(error);
       }
-    },
-  });
+    };
 
-  useEffect(() => {
-    if (feedback.length === 0) {
-      dispatch(ftechFeedback());
-    } else {
-      messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
+    getData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('https://65615e6adcd355c08323c948.mockapi.io/users', {
+        messages: newMessage,
+      });
+      setFeedback([...feedback, response.data]);
+      setNewMessage('');
+    } catch (error) {
+      console.log(error);
     }
-  }, [feedback]);
+  };
 
   return (
     <Layout>
       <main className='container-fulid'>
         <article className='row justify-content-center'>
           <section className='col-lg-8'>
-            <div className='card shadow mb-4' style={{ height: '85vh' }}>
+            <div className='card shadow mb-4' style={{height:'85vh'}}>
               <header className='card-header py-3 text-center'>
                 <h5 className='m-0 font-weight-bold text-orange'>Enjoy with our community</h5>
               </header>
-              <main className='card-body overflow-y-auto' ref={messagesContainerRef}>
+              <main className='card-body overflow-y-auto'>
                 {/* Use overflow-y-auto to enable vertical scrolling */}
                 {feedback.map((item) => (
                   <div key={item.id} className='mb-3 bg-grey'>
@@ -51,20 +50,16 @@ const Community = () => {
                   </div>
                 ))}
               </main>
-              <footer className='card-footer p-0 py-3 m-0'>
-                <form onSubmit={formik.handleSubmit}>
+              <footer className='card-footer p-0 py-1 m-0'>
+                <form onSubmit={handleSubmit}>
                   <fieldset className='form-group row justify-content-center m-0'>
-                    <div className='col-lg-7'>
-                      <input
-                        type='text'
-                        className='form-control'
-                        value={formik.values.messages}
-                        onChange={formik.handleChange}
-                        name='messages'
-                        id='messages'
-                      />
-                    </div>
-                    <button className='col-lg-1 btn btn-success' disabled={formik.values.messages === ''} type='submit'>
+                    <input
+                      type='text'
+                      className='form-control col-lg-6'
+                      value={newMessage}
+                      onChange={(e) => setNewMessage(e.target.value)}
+                    />
+                    <button className='col-lg-1 btn btn-success'>
                       <SendFill />
                     </button>
                   </fieldset>
